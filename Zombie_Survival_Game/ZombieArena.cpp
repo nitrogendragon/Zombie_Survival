9b58569b7275e26c15858f6c09a56ebbfc9c6342
nitrogendragon <dirtboy4fun@gmail.com>
2019-08-27 14:17:09 -0500

    Accidentally deleted state initizialization line in last commit but it's fixed now.

--- Zombie_Survival_Game/ZombieArena.cpp
+++ Zombie_Survival_Game/ZombieArena.cpp
@@ -18,7 +18,8 @@ int main()
 
 	// The game will always be in one of four states
 	enum class State { PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
-	
+	// Start with the GAME_OVER state
+	State state = State::GAME_OVER;
 
 	// Get the screen resolution and create an SFML window
 	Vector2f resolution;
